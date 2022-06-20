70cd9998d863baeaf8a3c894662dfff4d87413e0
DzakirinMD <dzakirinmd@gmail.com>
2022-06-20 12:17:27 +0800

    added Header,Footer and Main components

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+    return (
+        <header>
+        This is the Header
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/maincontent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContent(){
+    return (
+        <maincontent>
+        This is the MainContent
+        </maincontent>
+    )
+}
+
+export default MainContent
