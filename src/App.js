463f38dbca584c3ac7b9e75b0886da87db68a572
Dzakirin <m.abdrahim@dxc.com>
2022-07-08 18:13:04 +0800

    added lifecycle phase

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import ClassMainContent from './components/classmaincontent';
 import Footer from './components/footer';
 import Home from './components/home';
 import ApiCalls from './components/apicalls';
+import StateEventHandler from './components/stateeventhandler';
+import LifecycleDidMountPhase from './components/lifecyclephases/LifecycleDidMountPhase';
+import LifecycleUnmountPhase from './components/lifecyclephases/LifecycleUnmountPhase';
+import LifecycleComponentDidUpdatePhase from './components/lifecyclephases/LifecycleComponentDidUpdatePhase';
+
 
 // console.log(React.version);
 
@@ -18,11 +23,16 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          {/* passing props  pageTitle and numTopics to component */}
           <Route path="/" element={<Home pageTitle = {"Home Page"} />} />
           <Route path="/header" element={<Header />} />
           <Route path="/classmaincontent" element={<ClassMainContent pageTitle = {"Class Component Header"} numTopics = {5} />} />
           <Route path="/functionmaincontent" element={<FunctionMainContent pageTitle = {"Function Component Header"} numTopics = {10} />} />
           <Route path="/apicalls" element={<ApiCalls />} />
+          <Route path="/eventhandlers" element={<StateEventHandler />} />
+          <Route path="/lifecycledidmountphase" element={<LifecycleDidMountPhase initialNames={['Zack', 'Halia']} />} />
+          <Route path="/lifecycleunmountmountphase" element={<LifecycleUnmountPhase initialNames={['Ronald', 'Donald', 'Elise', 'Kim']} />} />
+          <Route path="/lifecyclecomponentdidupdatephase" element={<LifecycleComponentDidUpdatePhase />} />
           <Route path="/footer" element={<Footer />} />
         </Routes>
       </BrowserRouter>
--- src/components/home.js
+++ src/components/home.js
@@ -7,24 +7,50 @@ function Home(props){
         <div>
             <h2>{props.pageTitle}</h2>
             <table>
-                <tr>
-                    <th>Available URL </th>
-                </tr>
-                <tr>
-                    <td><a href="http://localhost:3000/header">Header</a></td>
-                </tr>
-                <tr>
-                    <td><a href="http://localhost:3000/classmaincontent">classmaincontent</a></td>
-                </tr>
-                <tr>
-                    <td><a href="http://localhost:3000/functionmaincontent">functionmaincontent</a></td>
-                </tr>
-                <tr>
-                    <td><a href="http://localhost:3000/apicalls">API Calls</a></td>
-                </tr>
-                <tr>
-                    <td><a href="http://localhost:3000/footer">Footer</a></td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th>Available URL</th>
+                        <th>Function</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><a href="http://localhost:3000/header">Header</a></td>
+                        <td>A header component</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/classmaincontent">classmaincontent</a></td>
+                        <td>A main component using CLASS component !</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/functionmaincontent">functionmaincontent</a></td>
+                        <td>A main component using FUNCTION component !</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/apicalls">API Calls</a></td>
+                        <td>A component showing how to call API (In Development)</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/eventhandlers">Event Handlers</a></td>
+                        <td>A component showing how use <b>Event</b></td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/lifecycledidmountphase">Lifecycle Did Mount</a></td>
+                        <td>A component showing how lifecycle <b>componentDidMount()</b> work</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/lifecycleunmountmountphase">Lifecycle Un-Mount</a></td>
+                        <td>A component showing how lifecycle <b>componentWillUnmount()</b> work</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/lifecyclecomponentdidupdatephase">Lifecycle Did/Should Update</a></td>
+                        <td>A component showing how lifecycle <b>componentDidUpdate()</b> work</td>
+                    </tr>
+                    <tr>
+                        <td><a href="http://localhost:3000/footer">Footer</a></td>
+                        <td>A footer component</td>
+                    </tr>
+                </tbody>
             </table> 
         </div>
     )
